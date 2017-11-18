7eb614c2b5614030f3aeb43ef05bf98ad4ec3454
Rizwan Reza <rizwanreza@gmail.com>
2017-11-18 01:51:32 -0600

    Checkpoint and assignment 9 player bar range inputs: completed

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class PlayerBar extends Component {
+  formatTime(time) {
+    const timeInSeconds = Math.floor(parseFloat(time))
+    const minutes = Math.floor(timeInSeconds / 60)
+    const remainingSeconds = timeInSeconds - (minutes * 60)
+    return minutes + ":" + remainingSeconds
+  }
+
   render() {
     return (
       <section className="player-bar">
@@ -15,10 +22,18 @@ class PlayerBar extends Component {
             <span className="ion-skip-forward"></span>
           </button>
         </section>
-        <section id="time-control">                                                                    
-          <div className="current-time">–:––</div>
-          <input type="range" className="seek-bar" value="0" />
-          <div className="total-time">–:––</div>
+        <section id="time-control">
+          <div className="current-time">{this.formatTime(this.props.currentTime)}</div>
+          <input
+            type="range"
+            className="seek-bar"
+            value={(this.props.currentTime / this.props.duration) || 0}
+            max="1"
+            min="0"
+            step="0.01"
+            onChange={this.props.handleTimeChange}
+          />
+          <div className="total-time">{this.formatTime(this.props.duration)}</div>
         </section>
         <section id="volume-control">
           <div className="icon ion-volume-low"></div>
