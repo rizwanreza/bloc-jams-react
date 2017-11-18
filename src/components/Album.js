209a63ed46a3f34561f700c11043c086baf144af
Rizwan Reza <rizwanreza@gmail.com>
2017-11-18 01:18:58 -0600

    Assignment 8 player bar buttons: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -45,6 +45,19 @@ class Album extends Component {
     }
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = currentIndex + 1
+
+    if (newIndex < this.state.album.songs.length) {
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play(newSong);
+    } else {
+      this.play(this.state.currentSong);
+    }
+  }
+
   handlePrevClick() {
     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
     const newIndex = Math.max(0, currentIndex - 1);
@@ -93,6 +106,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     )
